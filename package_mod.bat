7e3059d98b6b74fc9fadf470dbc8938db61c403e
c4_angel <20829444@qq.com>
2018-10-06 05:41:22 +0800

    package_mod

--- package_mod.bat
+++ package_mod.bat
@@ -7,10 +7,10 @@ setlocal
 call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 :: /* 修改一下4个变量，指定Mod的名字、版本、作者名字和Mod目录名，前3项将出现在Windows的安装程序界面，请勿使用中文 */
-set "mod_name=c4ImprovedKits"
-set mod_version=v1.0
-set mod_author=c4_angel
-set mod_folder=c4ImprovedKits
+set "mod_name=Improved Kits"
+set "mod_version=v1.0"
+set "mod_author=c4_angel"
+set "mod_folder=c4ImprovedKits"
 
 :: /* 从以下项目中选择Mod所兼容的游戏环境并修改：*/
 :: /* %bg1%, %bg2%, %bgt%, %tutu%, %bgee%, %bgiiee%, %iwd1%, %iwdee%, %iwd2%, %iwd2ee%, %iwd-in-bg2%, %pst%, %pstee%, %eet% */
@@ -25,8 +25,8 @@ set "compatible_games=%bgee%, %bgiiee%, or %eet%"
 set ico_folder=%mod_folder%\style
 set audio_folder=%mod_folder%\audio
 set tispack_folder=%mod_folder%\tiz
-set iconv_folder=%mod_folder%\languages\iconv
-set mod_readme=https://trow.cc/board/showforum=29897
+set iconv_folder=%mod_folder%\lang\iconv
+set mod_readme=https://trow.cc/board/showforum=37006
 
 :: /* 设置为不同系统打包安装文件，设为0表示不打包 */
 set build_windows=1
