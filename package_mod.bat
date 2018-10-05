19c0411b3bce12643cbb4d6fc2e959f8b1cb541e
c4_angel <20829444@qq.com>
2018-10-06 05:09:47 +0800

    readme.pdf

--- package_mod.bat
+++ package_mod.bat
@@ -7,10 +7,10 @@ setlocal
 call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 :: /* 修改一下4个变量，指定Mod的名字、版本、作者名字和Mod目录名，前3项将出现在Windows的安装程序界面，请勿使用中文 */
-set "mod_name=c4Monk"
-set mod_version=v4.2
+set "mod_name=c4ImprovedKits"
+set mod_version=v1.0
 set mod_author=c4_angel
-set mod_folder=c4Monk
+set mod_folder=c4ImprovedKits
 
 :: /* 从以下项目中选择Mod所兼容的游戏环境并修改：*/
 :: /* %bg1%, %bg2%, %bgt%, %tutu%, %bgee%, %bgiiee%, %iwd1%, %iwdee%, %iwd2%, %iwd2ee%, %iwd-in-bg2%, %pst%, %pstee%, %eet% */
